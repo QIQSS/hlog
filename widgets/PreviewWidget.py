896cbbc2292c6daa5988ed618979d5b986ba5416
Alexis Morel <mora0502@usherbrooke.ca>
2026-01-12 16:48:48 -0500

    preview better resize

--- widgets/PreviewWidget.py
+++ widgets/PreviewWidget.py
@@ -1,32 +1,49 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QSizePolicy
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
-
 
 class PreviewWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        # parent is the parent splitter
-        self.label = QLabel()
-        self.label.setAlignment(Qt.AlignCenter)
+
+        self.label = QLabel(alignment=Qt.AlignCenter)
+        self.label.setMinimumSize(0, 0)
+        self.label.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        
+        self._src_pixmap = None
+        self._last_size = None
 
         layout = QVBoxLayout(self)
         layout.addWidget(self.label)
 
     def showPng(self, png_bytes):
-        src = QPixmap()
-        src.loadFromData(png_bytes, "PNG")
+        pm = QPixmap()
+        pm.loadFromData(png_bytes, "PNG")
+        self._src_pixmap = pm
+        self._last_size = None
+        self._updatePixmap()
 
-        pixmap = QPixmap(self.label.size())
-        pixmap.fill(Qt.transparent)
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self._updatePixmap()
 
-        pixmap = src.scaled(
-            self.label.size(),
-            Qt.KeepAspectRatio,
-            Qt.SmoothTransformation,
+    def _updatePixmap(self):
+        if not self._src_pixmap:
+            return
+
+        size = self.label.size()
+        if size == self._last_size:
+            return
+
+        self._last_size = size
+        self.label.setPixmap(
+            self._src_pixmap.scaled(
+                size,
+                Qt.KeepAspectRatio,
+                Qt.SmoothTransformation,
+            )
         )
 
-        self.label.setPixmap(pixmap)
-
     def clear(self):
+        self._src_pixmap = None
         self.label.clear()
